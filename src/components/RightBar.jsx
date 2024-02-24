37dd88cdf0089b803157a14626043b8150c8a86c
nikhil <nikhilmahato3337@gmail.com>
2024-02-24 20:34:44 +0530

    online friend and activities section

--- src/components/RightBar.jsx
+++ src/components/RightBar.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const RightBar = () => {
   return (
-    <div className="w-[25%] sticky top-[72px] h-[calc(100vh-72px)] overflow-scroll scrollbar no-scrollbar">
+    <div className="w-[30%] sticky top-[72px] h-[calc(100vh-72px)] overflow-scroll scrollbar no-scrollbar">
       <div className="container p-5">
-        <div className=" shadow-md rounded-xl p-5">
+        <div className=" item shadow-md rounded-xl p-5 mb-5">
           <span className=" text-gray-400">Suggestions For You</span>
           <div className="user flex items-center justify-between my-5">
             <div className="userInfo flex items-center gap-5">
@@ -27,11 +27,165 @@ const RightBar = () => {
                 alt=""
                 className=" w-10 h-10 rounded-full object-cover"
               />
-              <span className=" font-medium">Hell nik</span>
+              <span className=" font-medium mr-[3px]">Hell nik</span>
             </div>
             <div className="buttons flex items-center gap-2">
               <button className=" p-1 text-white bg-blue-500">follow</button>
               <button className=" p-1 text-white bg-red-500">dismiss</button>
+            </div>
+          </div>
+        </div>
+        <div className=" item shadow-md rounded-xl p-5 mb-5">
+          <span className=" text-gray-400">Latest Activities</span>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <p className="text-gray-800 text-sm">
+                <span className=" font-medium text-black mr-[3px]">
+                  Hell nik
+                </span>
+                changed their cover picture
+              </p>
+            </div>
+            <span className="text-sm text-slate-400 "> 1 min ago</span>
+          </div>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <p className="text-gray-800 text-sm">
+                <span className=" font-medium text-black text-sm">
+                  Hell nik
+                </span>
+                changed their cover picture
+              </p>
+            </div>
+            <span className="text-sm text-slate-400">1 min ago</span>
+          </div>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <p className="text-gray-800 text-sm">
+                <span className=" font-medium text-black text-sm">
+                  Hell nik
+                </span>
+                changed their cover picture
+              </p>
+            </div>
+            <span className="text-sm text-slate-400">1 min ago</span>
+          </div>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <p className="text-gray-800 text-sm">
+                <span className=" font-medium text-black text-sm">
+                  Hell nik
+                </span>
+                changed their cover picture
+              </p>
+            </div>
+            <span className="text-sm text-slate-400">1 min ago</span>
+          </div>
+        </div>
+        <div className="item shadow-md rounded-xl p-5 mb-5">
+          <span className=" text-gray-400">Online Friends</span>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5 relative">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <div className="online w-3 h-3 rounded-full bg-green-500 absolute top-0 left-8"></div>
+
+              <span className=" font-medium text-black text-sm">Hell nik</span>
+            </div>
+          </div>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5 relative">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <div className="online w-3 h-3 rounded-full bg-green-500 absolute top-0 left-8"></div>
+
+              <span className=" font-medium text-black text-sm">Hell nik</span>
+            </div>
+          </div>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5 relative">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <div className="online w-3 h-3 rounded-full bg-green-500 absolute top-0 left-8"></div>
+
+              <span className=" font-medium text-black text-sm">Hell nik</span>
+            </div>
+          </div>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5 relative">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <div className="online w-3 h-3 rounded-full bg-green-500 absolute top-0 left-8"></div>
+
+              <span className=" font-medium text-black text-sm">Hell nik</span>
+            </div>
+          </div>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5 relative">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <div className="online w-3 h-3 rounded-full bg-green-500 absolute top-0 left-8"></div>
+
+              <span className=" font-medium text-black text-sm">Hell nik</span>
+            </div>
+          </div>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5 relative">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <div className="online w-3 h-3 rounded-full bg-green-500 absolute top-0 left-8"></div>
+
+              <span className=" font-medium text-black text-sm">Hell nik</span>
+            </div>
+          </div>
+          <div className="user flex items-center justify-between my-5">
+            <div className="userInfo flex items-center gap-5 relative">
+              <img
+                src="https://w0.peakpx.com/wallpaper/307/696/HD-wallpaper-ichigo-bleach-bleach.jpg"
+                alt=""
+                className=" w-10 h-10 rounded-full object-cover"
+              />
+              <div className="online w-3 h-3 rounded-full bg-green-500 absolute top-0 left-8"></div>
+
+              <span className=" font-medium text-black text-sm">Hell nik</span>
             </div>
           </div>
         </div>
